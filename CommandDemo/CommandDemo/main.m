f6eb29d0dcc7e10db197d1c3be774efa10e19132
pandazheng <543828315@qq.com>
2014-05-05 16:32:44 +0800

    Study IOS

---
+++ CommandDemo/CommandDemo/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  CommandDemo
+//
+//  Created by panda zheng on 14-5-5.
+//  Copyright (c) 2014年 panda zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int i = 100;
+        NSLog(@"%i",i);
+        
+        NSString *str = @"Hello World";
+        NSLog(@"%@",str);
+        
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
